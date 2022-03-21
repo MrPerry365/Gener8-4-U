60e9630790c514ac91952a90cf04f7b4089956b0
MrPerry365 <1987nowhere@gmail.com>
2022-03-20 20:29:07 -0400

    add character options and password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,23 @@
 // Assignment code here
 
 // add character options
+var characterChoice = {
+  special:'[!#$%&()*+-./:;<=>?@^{|}~]',
+  number:'0123456789',
+  upperCase:'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowerCase:'abcdefghijklmnopqrstuvwxyz',
+};
+
 // add function to generate the password
+
+function generatePassword() {
+  console.log('hello hello')
 // define password length
+  var pwdLength = prompt("Pick a number between 8 and 128 for your password!")
+
+}
+
+
 // prompt character choices
 // take character choices and randomize
 // turn randomized character choices into password
