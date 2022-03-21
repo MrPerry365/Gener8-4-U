4617bfc23634d7368881c8c7d98b252e00489db2
MrPerry365 <1987nowhere@gmail.com>
2022-03-20 20:48:43 -0400

    add character choices with prompts, and add array to put choices in

--- Develop/script.js
+++ Develop/script.js
@@ -11,14 +11,36 @@ var characterChoice = {
 // add function to generate the password
 
 function generatePassword() {
-  console.log('hello hello')
+ var passwordCharacterArray = "";
+
 // define password length
-  var pwdLength = prompt("Pick a number between 8 and 128 for your password!")
+  var pwdLength = prompt("Pick a number between 8 and 128 for your password!");
+
+// prompt character choices
+var special = confirm("Do you want the super secret password to have special characters it it?");
+if (special) {
+  passwordCharacterArray += characterChoice.special
+};
+
+var number = confirm("Do you want the super secret password to have numbers in it?");
+if (number) {
+  passwordCharacterArray += characterChoice.number
+};
+
+var upperCase = confirm("Do you want the super secret password to have upperCase characters in it?");
+if (upperCase) {
+  passwordCharacterArray += characterChoice.upperCase
+};
+
+var lowerCase = confirm("Do you want the super secret password to have lowerCase characters in it?");
+if (lowerCase) {
+  passwordCharacterArray += characterChoice.lowerCase
+};
 
 }
 
 
-// prompt character choices
+
 // take character choices and randomize
 // turn randomized character choices into password
 
